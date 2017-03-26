a17fa19d6288368eceb246bc61622598fbc7b5e8
neonfuz <neonfuz@gmail.com>
2017-03-26 18:56:51 -0500

    Added movement upon with A button press

--- src/main.c
+++ src/main.c
@@ -1,10 +1,29 @@
 #include <SDL2/SDL.h>
 #include <assert.h>
 
+// Software is being developed on a sytsem with terrible drivers that sometimes make neutral = 128, 129 deadzone should fix this.
+#define DEADZONE 129
+
 #define KEY_UP "k"
 #define KEY_DOWN "j"
 #define KEY_LEFT "h"
 #define KEY_RIGHT "l"
+
+static const
+char sdlToHack(int sdl_hat) {
+    ;switch(sdl_hat) {
+    case SDL_HAT_CENTERED: return '.';
+    case SDL_HAT_UP: return 'k';
+    case SDL_HAT_RIGHT: return 'l';
+    case SDL_HAT_DOWN: return 'j';
+    case SDL_HAT_LEFT: return 'h';
+    case SDL_HAT_RIGHTUP: return 'u';
+    case SDL_HAT_RIGHTDOWN: return 'n';
+    case SDL_HAT_LEFTUP: return 'y';
+    case SDL_HAT_LEFTDOWN: return 'b';
+    default: return '.';
+    }
+};
 
 #define UPDATE_FREQUENCY 1000
 
@@ -89,6 +108,7 @@ int main(int argc, char **argv)
             fprintf(stderr, "Couldn't open Joystick 0\n");
         }
     }
+    int hat = SDL_HAT_CENTERED;
 
     SDL_Event e;
     while (1) {
@@ -97,16 +117,41 @@ int main(int argc, char **argv)
             switch(e.type) {
             case SDL_QUIT: goto QUIT;
             case SDL_JOYAXISMOTION:
-                printf("Axis: %d, Value: %d\n", e.jaxis.axis, e.jaxis.value);
-//                switch(e.jaxis.axis) {
-//                }
+//                printf("Axis: %d, Value: %d\n", e.jaxis.axis, e.jaxis.value);
+                switch(e.jaxis.axis) {
+                case 0: // Horizontal
+                    if (e.jaxis.value > DEADZONE) { // Right
+                        hat &= SDL_HAT_UP | SDL_HAT_DOWN;
+                        hat |= SDL_HAT_RIGHT;
+                    } else if (e.jaxis.value < -DEADZONE) { // Left
+                        hat &= SDL_HAT_UP | SDL_HAT_DOWN;
+                        hat |= SDL_HAT_LEFT;
+                    } else {
+                        hat &= SDL_HAT_UP | SDL_HAT_DOWN;
+                    }
+                    break;
+                case 1: // Vertical
+                    if (e.jaxis.value > DEADZONE) { // Down
+                        hat &= SDL_HAT_LEFT | SDL_HAT_RIGHT;
+                        hat |= SDL_HAT_DOWN;
+                    } else if (e.jaxis.value < -DEADZONE) { // Up
+                        hat &= SDL_HAT_LEFT | SDL_HAT_RIGHT;
+                        hat |= SDL_HAT_UP;
+                    } else {
+                        hat &= SDL_HAT_LEFT | SDL_HAT_RIGHT;
+                    }
+                    break;
+                default: break;
+                }
                 break;
             case SDL_JOYBUTTONDOWN:
                 switch(e.jbutton.button) {
+                case 0:  printf("send-keys %c\n", sdlToHack(hat)); break;
                 case 11: printf("send-keys %s\n", KEY_UP); break;
                 case 12: printf("send-keys %s\n", KEY_DOWN); break;
                 case 13: printf("send-keys %s\n", KEY_LEFT); break;
                 case 14: printf("send-keys %s\n", KEY_RIGHT); break;
+                default: fprintf(stderr, "other button %d pressed.\n", e.jbutton.button);
                 }
                 break;
             default: break;
